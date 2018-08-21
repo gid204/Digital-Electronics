67e9afc490520911109d729abd14740bc9ab47c6
Unknown <gideon.kanikevich@me.com>
2018-08-21 13:36:23 +1000

    basic start
    started with some definitions and master initialisation

--- light_sensor/main/light_sensor.c
+++ light_sensor/main/light_sensor.c
@@ -19,6 +19,26 @@
 #include "driver/gpio.h"
 #include "driver/i2c.h"
 
+#define I2C_MASTER_SCL 					   18               /*!< GPIO for I2C master clock */
+#define I2C_MASTER_SDA                     19               /*!< GPIO for I2C master data */
+#define I2C_EXAMPLE_MASTER_NUM             I2C_NUM_1        /*!< I2C port number for master dev */
+#define I2C_EXAMPLE_MASTER_FREQ_HZ         100000           /*!< I2C master clock frequency */
+
+#define BH1750_SENSOR_ADDR                 0x23             /*!< slave address for BH1750 sensor */
+#define BH1750_CMD_START                   0x23             /*!< Command to set measure mode */
+#define ESP_SLAVE_ADDR                     0x28             /*!< ESP32 slave address, you can set any 7bit value */
+#define WRITE_BIT                          I2C_MASTER_WRITE /*!< I2C master write */
+#define READ_BIT                           I2C_MASTER_READ  /*!< I2C master read */
+#define ACK_CHECK_EN                       0x1              /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS                      0x0              /*!< I2C master will not check ack from slave */
+#define ACK_VAL                            0x0              /*!< I2C ack value */
+#define NACK_VAL                           0x1              /*!< I2C nack value */
+
+static void i2c_master_init()
+{
+	int i2c_master_port = I2C_EXAMPLE_MASTER_NUM;
+
+}
 
 void app_main()
 {
