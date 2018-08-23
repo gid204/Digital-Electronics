728ecec58b97163ca2180e90cbee8287164ded4c
Unknown <gideon.kanikevich@me.com>
2018-08-23 15:26:32 +1000

    Master Init

--- light_sensor/main/light_sensor.c
+++ light_sensor/main/light_sensor.c
@@ -19,10 +19,15 @@
 #include "driver/gpio.h"
 #include "driver/i2c.h"
 
-#define I2C_MASTER_SCL 					   18               /*!< GPIO for I2C master clock */
-#define I2C_MASTER_SDA                     19               /*!< GPIO for I2C master data */
+#define I2C_SLAVE_SCL_IO          		   26               /*!<gpio number for i2c slave clock  */
+#define I2C_SLAVE_SDA_IO           		   25               /*!<gpio number for i2c slave data */
+
+#define I2C_MASTER_SCL_IO 				   19               /*!< GPIO for I2C master clock */
+#define I2C_MASTER_SDA_IO                  18               /*!< GPIO for I2C master data */
 #define I2C_EXAMPLE_MASTER_NUM             I2C_NUM_1        /*!< I2C port number for master dev */
 #define I2C_EXAMPLE_MASTER_FREQ_HZ         100000           /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF                  0                /*!< I2C master does not need buffer */
+#define I2C_MASTER_RX_BUF                  0                /*!< I2C master does not need buffer */
 
 #define BH1750_SENSOR_ADDR                 0x23             /*!< slave address for BH1750 sensor */
 #define BH1750_CMD_START                   0x23             /*!< Command to set measure mode */
@@ -34,10 +39,22 @@
 #define ACK_VAL                            0x0              /*!< I2C ack value */
 #define NACK_VAL                           0x1              /*!< I2C nack value */
 
+/**
+ * @brief i2c master initialization
+ */
 static void i2c_master_init()
 {
 	int i2c_master_port = I2C_EXAMPLE_MASTER_NUM;
-
+	i2c.config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_SLAVE_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_EXAMPLE_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_EXAMPLE_MASTER_FREQ_HZ;
+    i2c_param_config(i2c_master_port, &conf);
+    i2c_driver_install(i2c_master_port, conf.mode,
+                       I2C_MASTER_RX_BUF, I2C_MASTER_TX_BUF, 0);
 }
 
 void app_main()
